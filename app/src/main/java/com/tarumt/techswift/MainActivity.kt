04d66c61f818ad838b966b445ac8fce48ee129ee
Caslu426 <lucaschong426@gmail.com>
2025-03-22 13:19:40 +0800

    Fix the api version problem
    Change api from 4.2.0 to 4.1.0

--- app/src/main/java/com/tarumt/techswift/MainActivity.kt
+++ app/src/main/java/com/tarumt/techswift/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tarumt.techswift.userUIScreen.UserScreenUI
 import com.tarumt.techswift.ui.theme.TechSwiftTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +15,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TechSwiftTheme {
-
+                UserScreenUI()
             }
         }
     }
+}
+
+@Composable
+@Preview
+fun PreviewUserUI(){
+    UserScreenUI()
 }
