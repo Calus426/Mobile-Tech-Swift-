94e24372a6b1b9a738994f91fb509093c9a28af9
Caslu426 <lucaschong426@gmail.com>
2025-04-15 11:08:06 +0800

    modify profile ui

--- app/src/main/java/com/tarumt/techswift/User/UiScreen/Profile/ProfileUI.kt
+++ app/src/main/java/com/tarumt/techswift/User/UiScreen/Profile/ProfileUI.kt
@@ -3,15 +3,24 @@ package com.tarumt.techswift.User.UiScreen.Profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,44 +29,80 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tarumt.techswift.R
+import com.tarumt.techswift.ui.theme.GreenBackground
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileUI(){
+fun ProfileUI() {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
         // Profile Image (overlapping top bar from MainScreen)
-        Image(
-            painter = painterResource(id = R.drawable.gem),
-            contentDescription = "Profile Image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(120.dp)
-                .align(Alignment.TopCenter)
-                .clip(CircleShape)
-                .border(4.dp, Color.White, CircleShape)
-        )
-        Box(
-            Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
 
-                // Main Content (push below the image)
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Box {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight(0.1f)
+                        .fillMaxWidth()
+                        .background(GreenBackground)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.gem),
+                    contentDescription = "Profile Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(120.dp)
+                        .align(Alignment.TopCenter)
+                        .clip(CircleShape)
+                        .border(4.dp, Color.White, CircleShape)
+                )
+
+            }
+
+            Box(
+                Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 80.dp, start = 24.dp, end = 24.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Spacer(modifier = Modifier.height(80.dp)) // space below image
 
-                    Text("Name: Gem", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+                    HorizontalDivider(modifier =  Modifier.padding(bottom = 3.dp, start = 10.dp, end = 10.dp))
+
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ){
+                        Text(
+                            "Name",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            textAlign = TextAlign.Start
+                        )
+                        OutlinedTextField(
+                            value = "Gem",
+                            onValueChange = {},
+                            placeholder = {Text("Gem")},
+                            colors = OutlinedTextFieldDefaults.colors(
+                              focusedContainerColor = Color(0xFFE5E1E1),
+                                unfocusedContainerColor = Color(0xFFE5E1E1)
+                            )
+                        )
+                    }
+
                     Spacer(modifier = Modifier.height(10.dp))
                     Text("Email: gem@example.com", fontSize = 18.sp)
 
@@ -66,6 +111,9 @@ fun ProfileUI(){
 
             }
         }
+
+
+    }
 
 
 }
